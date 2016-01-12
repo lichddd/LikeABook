d70b8f83cd9816cbd26ea5810c4d413b11ab5f40
木佐凪樹 <lichddd@126.com>
2016-01-12 16:15:36 +0800

    1

---
+++ js/LikeABook.js
@@ -0,0 +1,128 @@
+
+
+if ("undefined" == typeof jQuery) throw new Error("Bootstrap's JavaScript requires jQuery"); 
++(function ($) {
+
+	  
+	
+  var LikeABook   = function (el,option) {
+//  $(el).on('click',$.proxy(this.mychange,this));
+    var $book=$(el);
+    var $bookpages=$('<div class="bookpages"></div>');
+    var len=$book.children().length;
+    
+
+    
+    
+    for (var i = 0; i < Math.ceil(Number(len)/2); i++) {
+    	$bookpages.append('<div class="bookpage bookpage-right"></div>');
+    	
+    	if ($book.children()[1]) {
+    		$($book.children()[1]).addClass('bookback');
+    	$($bookpages.children()[i]).append($($book.children()[1]));
+    	
+    	} else{
+    		var $temp=$('<div></div>').addClass('bookback');
+    		$($bookpages.children()[i]).append($temp);
+    	}
+    	
+    	
+    	$($book.children()[0]).addClass('bookfront');
+    	$($bookpages.children()[i]).append($($book.children()[0]));
+    }
+    
+    
+
+     $bookpages.append($($bookpages.children().get().reverse()));
+    
+    
+    $bookpages.append('<div class="bookpage-left bookpage-feiye" style="z-index:-9999"><div class="bookback"></div><div class="bookfront"></div></div>')
+    $bookpages.prepend('<div class="bookpage-right bookpage-feiye"><div class="bookback"></div><div class="bookfront"></div></div>');
+    
+//  var $pages=$($book.children().get().reverse());
+    $book.addClass('book');
+    $book.append('<div class="bookcanvas"></div><div class="bookactivecanvas"><div class="bookactivecanvas-left"></div><div class="bookactivecanvas-right"></div></div>');
+    $bookpages.appendTo($book.children('.bookcanvas'));
+
+    
+    
+    $book.find('.bookactivecanvas').on('click','.bookactivecanvas-left',function (e) {
+			var $self=$($book.children('.bookcanvas').children('.bookpages').children('.bookpage.bookpage-left')[0]);
+//			$book.children('.bookcanvas').children('.bookpages').children().css('visibility','hidden');
+//			$($book.children('.bookcanvas').children('.bookpages').children('.bookpage-left')[1]).css('visibility','visible');
+//			var inx=$book.children('.bookcanvas').children('.bookpages').children('.bookpage-right').length-1;
+//			$($book.children('.bookcanvas').children('.bookpages').children('.bookpage-right')[inx]).css('visibility','visible');
+			
+			$self.css('z-index','');
+			
+			$self.removeClass('bookpage-left');
+			$self.addClass('bookpage-right');
+			setTimeout(function () {
+				$self.children($self.hasClass('bookpage-left')?'.bookback':'.bookfront').appendTo($self);
+			},250);
+//			$self.css('z-index','');
+		});
+	$book.find('.bookactivecanvas').on('click','.bookactivecanvas-right',function (e) {
+			var inx=$book.children('.bookcanvas').children('.bookpages').children('.bookpage.bookpage-right').length-1;
+			var $self=$($book.children('.bookcanvas').children('.bookpages').children('.bookpage.bookpage-right')[inx]);
+			
+//			$book.children('.bookcanvas').children('.bookpages').children().css('visibility','hidden');
+//			$($book.children('.bookcanvas').children('.bookpages').children('.bookpage-right')[inx-1]).css('visibility','visible');
+//			$($book.children('.bookcanvas').children('.bookpages').children('.bookpage-left')[0]).css('visibility','visible');
+//			
+
+
+			var $zindexself=$($book.children('.bookcanvas').children('.bookpages').children('.bookpage.bookpage-left')[0]);
+			var zindex=$book.children('.bookcanvas').children('.bookpages').children('.bookpage.bookpage-left').length-9999;
+			$self.removeClass('bookpage-right');
+			$self.addClass('bookpage-left');
+			
+			setTimeout(function () {
+				$self.children($self.hasClass('bookpage-left')?'.bookback':'.bookfront').appendTo($self);
+//				$self.css('z-index',$book.children('.bookcanvas').children('.bookpages').children('.bookpage-left').length-99);
+				$zindexself.css('z-index',zindex);
+			},250);
+			
+		});
+	
+	
+	
+	
+
+  }
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+  function Plugin(option) {
+    return this.each(function () {
+      var $this = $(this);
+      var data  = $this.data('my.likeABook');
+      var options = typeof option == 'object' && option;
+      if (!data) $this.data('my.likeABook', (data = new LikeABook(this,options)));
+      if (typeof option == 'string') data[option]()
+    })
+  }
+	
+	
+	$.fn.likeABook=Plugin;
+	$.fn.likeABook.Constructor=LikeABook;
+	
+	
+	
+$(window).on('load.my.likeABook.data-api', function () {
+    $('[data-likeABook="true"]').each(function () {
+      var $book = $(this)
+      Plugin.call($book)
+    })
+  })
+	
+})(jQuery)
+
---
+++ js/index.js
@@ -0,0 +1,121 @@
+$().ready(function () {
+
+function gradientColor(startColor,endColor,step){
+       startRGB = this.colorRgb(startColor);//转换为rgb数组模式
+       startR = startRGB[0];
+       startG = startRGB[1];
+       startB = startRGB[2];
+
+       endRGB = this.colorRgb(endColor);
+       endR = endRGB[0];
+       endG = endRGB[1];
+       endB = endRGB[2];
+
+       sR = (endR-startR)/step;//总差值
+       sG = (endG-startG)/step;
+       sB = (endB-startB)/step;
+
+       var colorArr = [];
+       for(var i=0;i<step;i++){
+		   //计算每一步的hex值 
+           var hex = this.colorHex('rgba('+parseInt((sR*i+startR))+','+parseInt((sG*i+startG))+','+parseInt((sB*i+startB))+',1)');
+           colorArr.push(hex);
+       }
+       return colorArr;
+   }
+
+   // 将hex表示方式转换为rgb表示方式(这里返回rgb数组模式)
+   gradientColor.prototype.colorRgb = function(sColor){
+       var reg = /^#([0-9a-fA-f]{3}|[0-9a-fA-f]{6})$/;
+       var sColor = sColor.toLowerCase();
+       if(sColor && reg.test(sColor)){
+           if(sColor.length === 4){
+               var sColorNew = "#";
+               for(var i=1; i<4; i+=1){
+                   sColorNew += sColor.slice(i,i+1).concat(sColor.slice(i,i+1));
+               }
+               sColor = sColorNew;
+           }
+           //处理六位的颜色值
+           var sColorChange = [];
+           for(var i=1; i<7; i+=2){
+               sColorChange.push(parseInt("0x"+sColor.slice(i,i+2)));
+           }
+           return sColorChange;
+       }else{
+           return sColor;
+       }
+   };
+   
+ gradientColor.prototype.colorHex = function(rgb){
+       var _this = rgb;
+       var reg = /^#([0-9a-fA-f]{3}|[0-9a-fA-f]{6})$/;
+       if(/^(rgb|RGB)/.test(_this)){
+           var aColor = _this.replace(/(?:(|)|rgb|RGB)*/g,"").split(",");
+           var strHex = "#";
+           for(var i=0; i<aColor.length; i++){
+               var hex = Number(aColor[i]).toString(16);
+               hex = hex<10 ? 0+''+hex :hex;// 保证每个rgb的值为2位
+               if(hex === "0"){
+                   hex += hex;
+               }
+               strHex += hex;
+           }
+           if(strHex.length !== 7){
+               strHex = _this;
+           }
+           return strHex;
+       }else if(reg.test(_this)){
+           var aNum = _this.replace(/#/,"").split("");
+           if(aNum.length === 6){
+               return _this;
+           }else if(aNum.length === 3){
+               var numHex = "#";
+               for(var i=0; i<aNum.length; i+=1){
+                   numHex += (aNum[i]+aNum[i]);
+               }
+               return numHex;
+           }
+       }else{
+           return _this;
+       }
+   }
+	
+	
+
+
+
+var pagenum=100;
+
+
+
+
+    	var gradient1 = new gradientColor('#E50743','#F9870F',pagenum);
+		var gradient2 = new gradientColor('#F9870F','#E8ED30',pagenum);
+		var gradient3 = new gradientColor('#E8ED30','#3FA62E',pagenum);
+		var gradient4 = new gradientColor('#3FA62E','#3BB4D7',pagenum);
+		var gradient5 = new gradientColor('#3BB4D7','#2F4D9E',pagenum);
+		var gradient6 = new gradientColor('#2F4D9E','#71378A',pagenum);
+		var gradients= gradient1.concat(gradient2).concat(gradient3).concat(gradient4).concat(gradient5).concat(gradient6);
+
+    
+
+for (var i = 0; i < 100; i++) {
+	$('#book').append('<div style="font-size: 96px;background-color:'+gradients[Math.floor(gradients.length*i/pagenum)]+'">'+i+'</div>');
+}
+		$('#book').likeABook();
+	
+	
+
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+})
+
